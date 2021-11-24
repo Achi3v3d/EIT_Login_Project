5c21447ce7e9ad6de30a69817487e1b2b48e74f9
Achi3v3d <udokwu.maduabuchi@gmail.com>
2021-11-24 04:55:58 -0500

    just need to fix the logo issue because I renamed it. main activity switching button should be workin

--- app/src/main/java/com/example/loginapp/MainActivity.kt
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.loginapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.loginapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.imageViewCreateAccount.setOnClickListener{
+            Intent(baseContext,MainActivity2::class.java).apply{
+                startActivity(this)
+            }
+
+        }
+    }
+
+    companion object{
+        const val LOG_TAG_CLICK = "LOG_TAG_CLICK"
     }
 }
