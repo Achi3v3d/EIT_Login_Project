9633c9ff85653d4e2b57dc810cf84acf41e4e2e2
Achi3v3d <udokwu.maduabuchi@gmail.com>
2021-11-24 06:28:10 -0500

    New Branch new Problems. Ive set all the elements I need for the app and figured out how im going to disable them or make them look disabled. now I just need to figure out how Im going to activily validate passwords and emails.
    Using this Validation password method Using utilities from java in Kotlin hoping this works.

--- app/src/main/java/com/example/loginapp/MainActivity2.kt
+++ app/src/main/java/com/example/loginapp/MainActivity2.kt
@@ -2,10 +2,28 @@ package com.example.loginapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.loginapp.databinding.ActivityMain2Binding
+import com.example.loginapp.databinding.ActivityMainBinding
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+    super.onResume()
+    }
+    fun isValidPassword(password: String?): Boolean {
+        val pattern: Pattern
+        val PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$"
+        pattern = Pattern.compile(PASSWORD_PATTERN)
+        val matcher: Matcher = pattern.matcher(password)
+        return matcher.matches()
     }
 }
